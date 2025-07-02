5000b14872c5a7952cf671dcb6870efb7140dd6a
Matheus <reboucas444@gmail.com>
2025-07-02 12:16:45 -0300

    getting chosen ticket

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 
 function comprar(){
+     //to take values type and amount
+     let getElement = document.getElementById.bind(document);
+     let type = getElement('tipo-ingresso');
+     let amount = getElement('qtd');
 
+     alert(`${type.value} , ${amount.value}`);
+
+     //reduce the amount of tickets
 }
