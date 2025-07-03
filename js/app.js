466cf126e88c0eb5a38ff37da8b2d9dd6c392127
Matheus <reboucas444@gmail.com>
2025-07-03 09:28:17 -0300

    include programming logic to buy track tickets

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
+let getElement = document.getElementById.bind(document);
 
 function comprar(){
      //to take values type and amount
-     let getElement = document.getElementById.bind(document);
      let type = getElement('tipo-ingresso');
-     let amount = getElement('qtd');
+     let amount = parseInt(getElement('qtd').value);
 
-     alert(`${type.value} , ${amount.value}`);
-
+     if(type.value == 'pista'){
+          comprarPista(amount);
+     }
      //reduce the amount of tickets
+}
+
+function comprarPista(amount) {
+     let amountTrackTickets = parseInt(getElement('qtd-pista').textContent);
+     
+     if(amount > amountTrackTickets) {
+          alert('Quantidade indisponivel para tipo pista');
+     } else {
+          amountTrackTickets = amountTrackTickets - amount;
+          getElement('qtd-pista').textContent = amountTrackTickets;
+          alert('Compra realizada com sucesso');
+     }
 }
