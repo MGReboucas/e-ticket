78dcfa75306fc7059004e70ba8ba9601c8e2284c
Matheus <reboucas444@gmail.com>
2025-07-03 09:44:43 -0300

    logic track, top and bottom tickets finished

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ function comprar(){
 
      if(type.value == 'pista'){
           comprarPista(amount);
+     } else if (type.value == 'superior') {
+          comprarSuperior(amount);
+     } else {
+          comprarInferior(amount);
      }
      //reduce the amount of tickets
 }
@@ -19,6 +23,30 @@ function comprarPista(amount) {
      } else {
           amountTrackTickets = amountTrackTickets - amount;
           getElement('qtd-pista').textContent = amountTrackTickets;
+          alert('Compra realizada com sucesso');
+     }
+}
+
+function comprarSuperior(amount) {
+     let amountTopTickets = parseInt(getElement('qtd-superior').textContent);
+
+     if(amount > amountTopTickets) {
+          alert('Quantidade indisponivel para tipo superior');
+     } else {
+          amountTopTickets = amountTopTickets - amount;
+          getElement('qtd-superior').textContent = amountTopTickets;
+          alert('Compra realizada com sucesso');
+     }
+}
+
+function comprarInferior(amount){
+     let amountBottomTickets = parseInt(getElement('qtd-inferior').textContent);
+     
+     if(amount > amountBottomTickets) {
+          alert('Quantidade indisponivel para tipo inferior');
+     } else {
+          amountBottomTickets = amountBottomTickets - amount;
+          getElement('qtd-inferior').textContent = amountBottomTickets;
           alert('Compra realizada com sucesso');
      }
 }
